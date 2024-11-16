c7a3e9a6ffef9bf97931af4b8603ca5ede06c9fe
uzayilica <uzayilica2@gmail.com>
2024-11-16 09:59:22 +0300

    ilk hali

---
+++ src/main/java/com/uzay/urun/UrunApplication.java
@@ -0,0 +1,13 @@
+package com.uzay.urun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uzay/urun/UrunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uzay.urun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
