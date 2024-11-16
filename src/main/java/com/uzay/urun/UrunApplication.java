aa946eb43f3971dfbf02b7b948a3b52bcf5baf98
uzayilica <uzayilica2@gmail.com>
2024-11-16 10:24:39 +0300

    properties yapılandırması tamamlandı

--- src/main/java/com/uzay/urun/UrunApplication.java
+++ src/main/java/com/uzay/urun/UrunApplication.java
@@ -1,13 +1,26 @@
 package com.uzay.urun;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UrunApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UrunApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UrunApplication.class, args);
+    }
+
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Welcome to Urun Application");
+        };
+    }
+
+
 
 }
